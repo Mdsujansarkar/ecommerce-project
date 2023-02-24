53c5ae0904fd790cec2e351c56351a011a5809e8
Mdsujansarkar <sujansarkar786@gmail.com>
2023-02-24 19:56:53 +0600

    blade templateing complite

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.backend');
+        return view('backend.home.home');
     }
 
     /**
