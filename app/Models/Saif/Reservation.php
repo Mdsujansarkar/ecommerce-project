ae1ec3dee91f40f346e4dafb8693e44f82833d96
Mdsujansarkar <sujansarkar786@gmail.com>
2023-03-13 00:08:49 +0600

    13min

---
+++ app/Models/Saif/Reservation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Saif;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Reservation extends Model
+{
+    use HasFactory;
+}
